9fb3aa1a6cd0ecbf2e8c03b179fef450abe0d57d
michal-klein_swi <michal.klein@solarwinds.com>
2024-04-30 19:41:02 +0200

    feat: setup repo

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const RootError = () => (
+  <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <h1 className="text-3xl font-bold">500</h1>
+    <p>Oops, unexpected error occurred</p>
+  </div>
+);
+
+export default RootError;
