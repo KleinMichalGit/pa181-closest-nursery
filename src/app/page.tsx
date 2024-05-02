3bec8b2fdf76ef3d81b344e85f20ae729c82f8a5
Michal Klein <kleinmichalgm@gmail.com>
2024-05-02 08:00:10 +0200

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,32 @@
 import { Nursery } from "@/types/nursery";
 
+async function fetchData(): Promise<Nursery> {
+  const response = await fetch(
+    "https://gis.brno.cz/ags1/rest/services/ODAE/ODAE_zapis_ms/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson",
+  );
+
+  return response.json();
+}
+
 export default async function Home() {
-  const data: Nursery = await (
-    await fetch(
-      "https://gis.brno.cz/ags1/rest/services/ODAE/ODAE_zapis_ms/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson",
-    )
-  ).json();
+  const data = await fetchData();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>PA181</h1>
-      {data.features.map((feature: any) => (
-        <div
-          key={feature.properties.title}
-          className="p-4 m-4 border border-gray-300"
-        >
-          <h2>{feature.properties.address}</h2>
-          <p>{feature.properties.telephone}</p>
-          <p>{feature.properties.longitude}</p>
-          <p>{feature.properties.latitude}</p>
-          <p>{feature.properties.email}</p>
-        </div>
-      ))}
+      {data.features.map(
+        ({
+          properties: { title, address, telephone, longitude, latitude, email },
+        }) => (
+          <div key={title} className="p-4 m-4 border border-gray-300">
+            <h2>{address}</h2>
+            <p>{telephone}</p>
+            <p>{longitude}</p>
+            <p>{latitude}</p>
+            <p>{email}</p>
+          </div>
+        ),
+      )}
     </main>
   );
 }
