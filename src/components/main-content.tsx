95534bb628f3a5549f4469e16d07bcbfe8b6e05c
Michal Klein <kleinmichalgm@gmail.com>
2024-06-11 16:56:16 +0200

    Update main-content.tsx

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -1,48 +1,44 @@
 "use client";
 
-import { useState } from "react";
-import Navigation from "@/components/navigation/navigation";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
-import { ClosestSchoolType, MapType } from "@/types/map-type";
+import Navigation from "@/components/navigation/navigation";
 import SideMenu from "@/components/navigation/side-menu";
+import { ClosestSchoolType, MapType } from "@/types/map-type";
 
 const Map = dynamic(() => import("../components/map"), { ssr: false });
 
-const MainContent = ({ schools }: MapType) => {
+const MainContent: React.FC<MapType> = ({ schools }) => {
   const [filter, setFilter] = useState("");
   const [closestSchools, setClosestSchools] = useState<
     ClosestSchoolType[] | null
   >(null);
 
-  const filteredSchools =
-    filter === ""
-      ? schools
-      : schools.filter(
-          (school) =>
-            school.properties.address
-              .toLowerCase()
-              .includes(filter.toLowerCase()) ||
-            school.properties.title
-              .toLowerCase()
-              .includes(filter.toLowerCase()) ||
-            school.properties.telephone
-              .toLowerCase()
-              .includes(filter.toLowerCase()) ||
-            school.properties.email
-              .toLowerCase()
-              .includes(filter.toLowerCase()) ||
-            school.properties.director
-              .toLowerCase()
-              .includes(filter.toLowerCase()) ||
-            school.properties.website
-              .toLowerCase()
-              .includes(filter.toLowerCase()),
-        );
+  const handleFilterChange = (filterValue: string) => {
+    setFilter(filterValue);
+  };
+
+  const filteredSchools = schools.filter((school) => {
+    const { address, title, telephone, email, director, website } =
+      school.properties;
+    const filterLower = filter.toLowerCase();
+    return (
+      address.toLowerCase().includes(filterLower) ||
+      title.toLowerCase().includes(filterLower) ||
+      telephone.toLowerCase().includes(filterLower) ||
+      email.toLowerCase().includes(filterLower) ||
+      director.toLowerCase().includes(filterLower) ||
+      website.toLowerCase().includes(filterLower)
+    );
+  });
 
   return (
     <>
-      <Navigation setFilter={setFilter} closestSchools={closestSchools} />
-      <main className="block md:flex justify-between mx-auto pl-4 pr-4 z-0 relative">
+      <Navigation
+        setFilter={handleFilterChange}
+        closestSchools={closestSchools}
+      />
+      <main className="block md:flex justify-between mx-auto px-4 z-0 relative">
         <SideMenu closestSchools={closestSchools} />
         <Map schools={filteredSchools} setClosestSchools={setClosestSchools} />
       </main>
